73ef9c14e72a3c6e69ee2c42f474751f8459fc11
paul-njeze <paul.njeze@gmail.com>
2026-06-19 21:05:13 -0400

    Legal: /acceptable-use page

---
+++ gigwrench/app/acceptable-use/page.tsx
@@ -0,0 +1,7 @@
+import LegalDoc from '@/components/legal/LegalDoc'
+
+export const metadata = { title: 'Acceptable Use Policy - GigWrench' }
+
+export default function Page() {
+  return <LegalDoc slug="acceptable-use" />
+}
