1700ba5b920c2a9703e317cb63a51b777073595f
paul-njeze <paul.njeze@gmail.com>
2026-06-19 21:05:12 -0400

    Legal: /privacy page

---
+++ gigwrench/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+import LegalDoc from '@/components/legal/LegalDoc'
+
+export const metadata = { title: 'Privacy Policy - GigWrench' }
+
+export default function Page() {
+  return <LegalDoc slug="privacy" />
+}
